7cb351ba45603578a753851bd25cd4275581efbd
baskhuu <baskhuu.fkc@gmail.com>
2025-05-22 10:19:32 +0900

    Loading screen

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "fkc.llc.fkc1"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk =  flutter.minSdkVersion             // 21
+        targetSdk = flutter.targetSdkVersion        // 30
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
